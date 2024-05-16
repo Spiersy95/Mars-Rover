f39571bfe8672896bb0bf5a91406ae241414f684
Spiersy95 <scottspeirsstirling@gmail.com>
2024-05-16 11:38:57 +0100

    Missing Input tests passed

--- src/test/java/PlateauParserTest.java
+++ src/test/java/PlateauParserTest.java
@@ -44,4 +44,30 @@ class PlateauParserTest {
         assertFalse(PlateauParser.plateauFormatVerify.test(input3));
         assertFalse(PlateauParser.plateauFormatVerify.test(input4));
     }
+
+    @Test
+    void missingInputPlateauFormatVerifyTest(){
+        String input1 = "1 ";
+        String input2 = "32";
+        String input3 = " 2320";
+        String input4 = "323 03";
+
+
+        assertFalse(PlateauParser.plateauFormatVerify.test(input1));
+        assertFalse(PlateauParser.plateauFormatVerify.test(input2));
+        assertFalse(PlateauParser.plateauFormatVerify.test(input3));
+        assertFalse(PlateauParser.plateauFormatVerify.test(input4));
+    }
+
+    @Test
+    void extraInputPlateauFormatVerifyTest(){
+        String input1 = "55 23";
+        String input2 = "50 20";
+        String input3 = "2 1000";
+
+        assertTrue(PlateauParser.plateauFormatVerify.test(input1));
+        assertTrue(PlateauParser.plateauFormatVerify.test(input2));
+        assertTrue(PlateauParser.plateauFormatVerify.test(input3));
+    }
+
 }
